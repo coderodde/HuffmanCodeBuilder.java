bac5252dfc085bed571fac56217d75315894f52a
Rodion Efremov <coderodd3@yandex.com>
2025-11-16 15:57:20 +0200

    Added ByteWeightDistributionBuilder.java.

---
+++ src/main/java/io/github/coderodde/compressor/app/ByteWeightDistributionBuilder.java
@@ -0,0 +1,54 @@
+package io.github.coderodde.compressor.app;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This class provides a method for building instances of
+ * {@link io.github.coderodde.compressor.app.WeightDistribution} over byte-wise
+ * data.
+ * 
+ * @author Rodion "rodde" Efremov
+ * @version 1.0.0 (Nov 16, 2025)
+ * @since 1.0.0 (Nov 16, 2025)
+ */
+public final class ByteWeightDistributionBuilder {
+
+    private static final int BYTE_ALPHABET_SIZE = 256;
+    
+    private ByteWeightDistributionBuilder() {
+        
+    }
+    
+    /**
+     * Builds and returns the weight distribution of the input raw data.
+     * 
+     * @param rawData the byte array holding the data to compress.
+     * 
+     * @return the weight distribution.
+     */
+    public static WeightDistribution<Byte> 
+        buildByteWeightDistribution(final byte[] rawData) {
+        
+        final WeightDistribution<Byte> weightDistribution =
+                new WeightDistribution<>();
+        
+        final Map<Byte, Integer> frequencyMap = 
+                new HashMap<>(BYTE_ALPHABET_SIZE);
+        
+        for (int i = Byte.MIN_VALUE; i <= Byte.MAX_VALUE; ++i) {
+            frequencyMap.put((byte) i, 0);
+        }
+        
+        for (final byte b : rawData) {
+            frequencyMap.put(b, frequencyMap.get(b) + 1);
+        }
+        
+        for (final Map.Entry<Byte, Integer> entry : frequencyMap.entrySet()) {
+            weightDistribution.associateSymbolWithWeight(entry.getKey(),
+                                                         entry.getValue());
+        }
+        
+        return weightDistribution;
+    }
+}
